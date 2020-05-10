a81a67214a300550bf4cd8b91c6e3d7163dcbb93
MateuszBelka <belka.mateusz.bm@gmail.com>
2020-05-10 21:15:44 +0200

    Implement client-side routing for gh pages.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'; // Using HashRouter due to lack of pushState support on GH pages
+
+import Home from './components/Home';
+import About from './components/About';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-    <h1>Hello World</h1>
-    </div>
-  );
+/*
+  Implement HashRouting to allow subdirectories on gh pages
+  https://levelup.gitconnected.com/deploying-a-create-react-app-with-routing-to-github-pages-f386b6ce84c2
+  https://reacttraining.com/react-router/
+*/
+
+class App extends Component {
+  state = {
+
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about/">
+            <About />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
